046eee4e537a708395efb051d0b4ec89960de27a
zhaobao1830 <x5219438>
2018-07-25 00:22:25 +0800

    1

---
+++ application/sample/controller/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/24
+ * Time: 23:17
+ */
+
+namespace app\sample\controller;
+
+
+class Test
+{
+  public function hello()
+  {
+      return 'Hello,zb';
+  }
+}
