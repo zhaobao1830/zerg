a4ecdf3e57420e3af2d70db67fa8fa787b782294
zhaobao1830 <373098777@qq.com>
2018-11-07 17:25:14 +0800

    添加令牌检测

--- application/api/service/Token.php
+++ application/api/service/Token.php
@@ -105,4 +105,14 @@ class Token
         }
         return false;
     }
+
+    public static function verifyToken($token){
+        $exist = Cache::get($token);
+        if($exist){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
--- application/route.php
+++ application/route.php
@@ -61,6 +61,7 @@ Route::get('api/:version/product/:id', 'api/:version.Product/getOne',[],['id'=>'
 Route::get('api/:version/category/all', 'api/:version.Category/getAllCategories');
 
 Route::post('api/:version/token/user', 'api/:version.Token/getToken');
+Route::post('api/:version/token/verify', 'api/:version.Token/verifyToken');
 
 Route::post('api/:version/address', 'api/:version.Address/createOrUpdateAddress');
 
