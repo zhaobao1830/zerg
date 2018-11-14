321ba578cf3deb3db91ca53f7a7ed1a4ad631aa1
zhaobao1830 <x5219438>
2018-11-14 22:28:25 +0800

    修改Base 验证器

--- application/api/validate/BaseValidate.php
+++ application/api/validate/BaseValidate.php
@@ -30,18 +30,22 @@ class BaseValidate extends Validate
     public function goCheck()
     {
         //获取http传入的参数
-        // 对这些参数进行校验
+        //对这些参数做校验
         $request = Request::instance();
         $params = $request->param();
-
-        $result = $this->check($params);
-        if(!$result){
-//            $e = new ParameterException([
-            $error = $this->error;
-//            ]);
-            throw new Exception($error);
-//            throw $msg;
-        }else{
+        $result = $this->batch()->check($params);
+        if (!$result) {
+            $e = new ParameterException([
+                'msg' => $this->error,
+                // 'code' => 400,
+                // 'errorCode' => 10002
+            ]);
+            // $e->msg = $this->error;
+            // $e->errorCode = 10002;
+            throw $e;
+            // $error = $this->error;
+            // throw new Exception($error);
+        } else {
             return true;
         }
     }
